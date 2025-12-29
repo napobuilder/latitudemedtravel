3fd772ed1c6fc764a5dd414b2dbd35ea290d3852
Napoleon <napoacademy@gmail.com>
2025-12-29 19:14:18 -0400

    Actualizar nÃºmero de telÃ©fono en footer
    - Cambiar de +1 (616) 274-8519 a +1 (423) 482-1525

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -47,7 +47,7 @@ const Footer: React.FC = () => {
               </li>
               <li className="flex items-center">
                 <svg className="w-4 h-4 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20"><path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" /></svg>
-                <span>+1 (616) 274-8519</span>
+                <span>+1 (423) 482-1525</span>
               </li>
             </ul>
           </div>
